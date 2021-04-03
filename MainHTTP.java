e71dbb34b7a748d16f4056cb272f5d93a0471de4
0xRar <33517160+0xRar@users.noreply.github.com>
2021-04-04 01:53:17 +0300

    Update MainHTTP.java

--- MainHTTP.java
+++ MainHTTP.java
@@ -6,8 +6,7 @@ import java.net.http.HttpResponse;
 
 public class MainHTTP {
     public static void main(String[] args) {
-        // java.net.http.HttpClient
-
+        
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/users")).build();
 
